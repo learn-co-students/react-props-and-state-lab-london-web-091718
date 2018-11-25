96f8fba0842b776d94bf8d4edbb111b79c9eda40
anavarreteuk <a.navarrete.uk@gmail.com>
2018-11-25 20:28:21 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
@@ -15,6 +14,32 @@ class App extends React.Component {
     }
   }
 
+  adoptPet = petToChange => {
+    const pets = [...this.state.pets]
+    const foundPet = pets.find(pet => pet.id === petToChange.id)
+    const foundPetCopy = JSON.parse(JSON.stringify(foundPet))
+    foundPetCopy.isAdopted = true
+    const index = pets.indexOf(petToChange)
+    pets[index] = foundPetCopy
+    this.setState({ pets})
+  }
+
+  updateFilter = newFilter => {
+    this.setState({ filters: { type: newFilter }})
+  }
+
+  getPets = () => {
+    const type = this.state.filters.type
+    const url = type === 'all'?
+    '/api/pets':
+    `/api/pets?type=${type}`
+
+
+    fetch(url)
+      .then(resp => resp.json())
+      .then(pets => this.setState({ pets }))
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +49,11 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters updateFilter={this.updateFilter}
+              getPets= {this.getPets} />
             </div>
-            <div className="twelve wide column">
-              <PetBrowser />
+            <div className="twe{lve wide column">
+              <PetBrowser adoptPet={this.adoptPet} pets= {this.state.pets} />
             </div>
           </div>
         </div>
